1843cf932f62861ae3711485c0bd68682a7a22bb
Derek Drackley <derekdrackley@gmail.com>
2022-03-24 07:42:48 -0500

    HackerRank JS - Divisible Sum Pairs

---
+++ HackerRank/JavaScript/Easy/Divisible-Sum-Pairs.js
@@ -0,0 +1,15 @@
+/*
+  Solution by TribalK
+*/
+function divisibleSumPairs(n, k, ar) {
+    let sumPairsFound = 0;
+    for(let i=0; i<n; i++) {
+        for(let j=i+1; j<n; j++) {
+            if((ar[i]+ar[j]) % k === 0) {
+                sumPairsFound++;
+            }
+        }
+    }
+
+    return sumPairsFound;
+}
