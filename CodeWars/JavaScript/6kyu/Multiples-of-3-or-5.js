bc0bf956c9ba59a7a429d11cc2611329b91fcdf5
Derek Drackley <derekdrackley@gmail.com>
2022-04-07 00:31:30 -0500

    Codewars 6kyu - Multiples of 3 or 5

---
+++ CodeWars/JavaScript/6kyu/Multiples-of-3-or-5.js
@@ -0,0 +1,14 @@
+/*
+  Solution by TribalK
+*/
+function solution(number){
+  let total = 0;
+
+  for(let i=3; i<number; i++) {
+    if(i % 3 === 0 || i % 5 === 0) {
+        total += i;
+    }
+  }
+
+  return total;
+}
