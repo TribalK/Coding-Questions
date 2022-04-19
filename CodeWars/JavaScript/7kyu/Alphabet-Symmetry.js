120cbf6a1199236db83296b01255e46aea087a7b
Derek Drackley <derekdrackley@gmail.com>
2022-04-19 01:36:50 -0500

    CodeWars 7kyu JS - Alphabet Symmetry

--- CodeWars/JavaScript/7kyu/Alphabet-Symmetry.js
+++ CodeWars/JavaScript/7kyu/Alphabet-Symmetry.js
@@ -2,11 +2,11 @@
   Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word.
 
   Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
-  
+
   Solution by TribalK
 */
 function solve(arr){
-  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   let symmetryArr = [];
 
   for(let i=0; i<arr.length; i++) {
